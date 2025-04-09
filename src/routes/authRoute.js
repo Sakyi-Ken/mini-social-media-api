cfa181de5f2673b9688f46c938b6ce8468baff32
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-09 23:04:39 +0000

    Set up sign up route

---
+++ src/routes/authRoute.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const { signUp } = require('../controllers/authController');
+
+
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     description: Signup a new user
+ *     parameters:
+ *       - name: first_name
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: last_name
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: username
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: email
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         in: body
+ *         required: true
+ *         type: string
+ *       - name: phone_number
+ *         in: body
+ *         required: false
+ *         type: string
+ *       - name: date_of_birth
+ *         in: body
+ *         required: false
+ *         type: string 
+ *
+ *     responses:
+ *      201:
+ *        description: User created successfully
+ * *      400:
+ *       description: Bad request, user already exists or missing fields
+ * *      500:
+ *       description: Internal server error
+ * *     produces:
+ * *       - application/json
+ *     consumes:
+ * *       - application/json   
+ * */
+
+// @route    POST /api/auth/signup
+// @desc     Register a new user
+// @access   Public
+router.post('/signup', signUp);
+
+
+module.exports = router;
