3ae097ad4b803facfaa71cbbee88b1ba4869a5b7
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-10 13:07:15 +0000

    Create auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+const auth = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: "No token provided. Access denied." });
+    }
+
+    const token = authHeader.split(" ")[1];
+    // verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach user info to the request
+    req.user = await User.findById(decoded.id).select('-password');
+
+    next(); // move to next middleware or roue handler
+  } catch (err) {
+    console.error("Invalid or expired token", err.message);
+    res.status(500).json({ message: "Internal Server Error", success: false});
+  }
+};
+
+module.exports = auth;
