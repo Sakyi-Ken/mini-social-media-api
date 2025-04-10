14b4515a9d2925fdc4047548f3f0f450e9f83810
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-04-10 23:17:12 +0000

    create the api docs for auth routes.

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -7,57 +7,156 @@ const { signUp, login } = require('../controllers/authController');
  * @swagger
  * /api/auth/signup:
  *   post:
- *     tags:
- *       - Auth
- *     description: Signup a new user
- *     parameters:
- *       - name: first_name
- *         in: body
- *         required: true
- *         type: string
- *       - name: last_name
- *         in: body
- *         required: true
- *         type: string
- *       - name: username
- *         in: body
- *         required: true
- *         type: string
- *       - name: email
- *         in: body
- *         required: true
- *         type: string
- *       - name: password
- *         in: body
- *         required: true
- *         type: string
- *       - name: phone_number
- *         in: body
- *         required: false
- *         type: string
- *       - name: date_of_birth
- *         in: body
- *         required: false
- *         type: string 
- *
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - first_name
+ *               - last_name
+ *               - username
+ *               - email
+ *               - password
+ *             properties:
+ *               first_name:
+ *                 type: string
+ *               last_name:
+ *                 type: string
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
  *     responses:
- *      201:
- *        description: User created successfully
- * *      400:
- *       description: Bad request, user already exists or missing fields
- * *      500:
- *       description: Internal server error
- * *     produces:
- * *       - application/json
- *     consumes:
- * *       - application/json   
- * */
+ *       201:
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object 
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                     first_name:
+ *                       type: string
+ *                     last_name:
+ *                       type: string
+ *                     username:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *                     password:
+ *                       type: string
+ *                     phone_number:
+ *                       type: string
+ *                     date_of_birth:
+ *                       type: string
+ *       400:
+ *         description: Invalid input
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+*/
 
 // @route    POST /api/auth/signup
 // @desc     Register a new user
 // @access   Public
 router.post('/signup', signUp);
 
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Logged in successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 accessToken:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                     username:
+ *                       type: string
+ *                     email:
+ *                       type: string
+ *       400:
+ *         description: Invalid credentials
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object 
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+*/
 
 // @route    POST /api/auth/login
 // @desc     Login a user
